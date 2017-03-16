eb10de0370b8c44b14c287d7657a0acf1a24a7bc
Kristin <kvcmorin@gmail.com>
2017-03-16 20:50:53 +0000

    added delete + update buttons that grab correct properties from the 'body' and update them and also give appropiate redirects

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
-//when do we make libraries a function so we can access all their methods
+//how does making libraries a function give us access to all their methods
 
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
@@ -26,7 +26,7 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls_index", (req, res) => {
+app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -36,34 +36,36 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL,
+                      longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-//you're rendering above template and using the keys and vlues in other files
-//'urls_show' is the template name. call that name to make use of the template
-//we're calling shortURL + longURL the keys and printing their values to the browser
-
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  // // Respond with 'Ok' (we will replace this)
-  // res.send("Ok");
   let longURL = urlDatabase[shortURL];
   res.redirect(longURL);
-  // res.redirect("/urls/" + shorttURL);
 });
 //make sure above app.post correspends to the form attribute method in /urls path file
 //express routes are based on paths AND methods!!!
 
-app.get("/urls", (req, res) => {
-  res.render("urls_index", { urls: urlDatabase });
-});
-
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  console.log(req);
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  var newURL = req.body["longURL"];
+  urlDatabase[req.params.shortURL] = newURL;
+  res.redirect("/urls");
 });
 
 
